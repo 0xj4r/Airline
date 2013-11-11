2fdf9fe86aab046a3803cf83cc18953318598f66
Josh Ransom <josh.bh.ransom@gmail.com>
2013-11-11 10:59:03 -0700

    removed files

--- src/dbconnect.php
+++
@@ -1,4 +0,0 @@
-<?php
-$dbCon = mysqli_connect("localhost", "root", "", "airlineaccounts") or die("cannot connect");
-//Parameters: server, username (default is root in XAMPP), password (XAMPP doesn't set), database name
-?>
