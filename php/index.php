267ed4c2a47b9b90f1f51e74db2f5fb6ddddb238
dvanica <dvanica@users.noreply.github.com>
2013-11-17 21:28:24 -0700

    Linked to menu.html

--- php/index.php
+++ php/index.php
@@ -56,69 +56,9 @@ if(isset($_POST['username'])) {
 	<link type="text/css"  rel="stylesheet" href="RSSstyles.css"  />
 </head>
 <body>
-<!-------------------------------------
-			MENU FIELDS 
-			--------------------------------------------->
-<div id = "menu">
-<ul id="sdt_menu" class="sdt_menu">
-    <li>
-        <a href="index.php">
-            <img src="./images/1.jpg" alt=""/>
-            <span class="sdt_active"></span>
-            <span class="sdt_wrap">
-                <span class="sdt_link">Home</span>
-                <span class="sdt_descr"></span>
-            </span>
-        </a>
-    </li>
-	<li>
-        <a href="#">
-            <img src="./images/2.jpg" alt=""/>
-            <span class="sdt_active"></span>
-            <span class="sdt_wrap">
-                <span class="sdt_link">Get Away</span>
-                <span class="sdt_descr">Search for Flights</span>
-            </span>
-        </a>
-    </li>
-	<li>
-        <a href="#">
-            <img src="./images/3.jpg" alt=""/>
-            <span class="sdt_active"></span>
-            <span class="sdt_wrap">
-                <span class="sdt_link">Deals</span>
-                <span class="sdt_descr">Featured Flights</span>
-            </span>
-        </a>
-    </li>
-	<li>
-        <a href="#">
-            <img src="./images/4.jpg" alt=""/>
-            <span class="sdt_active"></span>
-            <span class="sdt_wrap">
-                <span class="sdt_link">Contact Us</span>
-                <span class="sdt_descr">We want to hear from you</span>
-            </span>
-        </a>
-    </li>
-	<li>
-        <?php  
-		echo "<a href = \"" .$menuLink. "\">";
-		?>
-            <img src="./images/5.jpg" alt=""/>
-            <span class="sdt_active"></span>
-            <span class="sdt_wrap">
-                <span class="sdt_link">My Account</span>
-                <span class="sdt_descr">Manage your Account</span>
-            </span>
-        </a>
-    </li>
-</ul>
-</div>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-<script type="text/javascript" src="Javascript/jquery.easing.1.3.js"></script>
-<script type="text/javascript" src="Javascript/menu.js"></script>
-<!-- END MENU FIELDS -->
+<?php
+	require_once("menu.html");
+	?>
 <!-------------------------------------
 			LOGIN FORM 
 		--------------------------------------------->
