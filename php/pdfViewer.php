92c1682056eb4852f342592579c118143dce730b
amazzola <mazzolaamy@cox.net>
2013-11-18 20:57:41 -0700

    updated pdfViewer.php

--- php/pdfViewer.php
+++ php/pdfViewer.php
@@ -1,6 +1,6 @@
 <?php
-session_start(); 
-include_once("dbconnect.php");	//Connects to database
+session_start();
+include_once("dbconnect.php");
 error_reporting(E_ALL);
 set_time_limit(1800);
 set_include_path('../src/' . PATH_SEPARATOR . get_include_path());
@@ -15,86 +15,50 @@ class Creport extends Cezpdf{
 $pdf = new Creport('a4','portrait');
 // to test on windows xampp
   if(strpos(PHP_OS, 'WIN') !== false){
-    $pdf->tempPath = 'C:\xampp\htdocs\Airlinenewnew/Airline-master';
+        $pdf->tempPath = 'C:\xampp\htdocs\newnewnew/Airline-master';
   }
   
 $pdf->ezSetMargins(20,20,20,20);
 $pdf->openHere('Fit');
 
-$pdf->selectFont('../src/fonts/Helvetica.afm');
-$pdf->ezText("Text in Helvetica");
-$pdf->selectFont('../src/fonts/Courier.afm');
-$pdf->ezText("Text in Courier");
-$pdf->selectFont('../src/fonts/Times-Roman.afm');
-$pdf->ezText("Text in Times New Roman");
 
-//$flightNum=$_SESSION['flight_num'];
-
-//$mysql = "SELECT * FROM flights WHERE flight_num = '1' LIMIT 1";
-	//$result = mysqli_query($dbCon, $mysql);
-	//$row = mysqli_fetch_row($result);
-	//$dbDepartCity = $row[0];
-	//$dbDepartSt = $row[1];
-	//$dbDepartTime = $row[2];
-    //$dbArrivalCity = $row[3]; 
-	//$dbArrivalSt= $row[4]; 
-	//$dbFlightDuration=$row[5];
+$pdf->selectFont('../src/fonts/Helvetica');
+$pdf->ezText("Reciept\n");
 
 
-
-//$data=array();
-
-//if($flightNum!=NULL)
-//{
-//$result=mysqli_query($dbCon,"SELECT * FROM flights WHERE flight_num='1'");
-// WHERE flight_num=$flightNum
-
-//$pdf->selectFont('./fonts/Helvetica');
-//$pdf->addText(30,400,30,'hello');
-//$pdf->addText(30,400,30,$dbDepartCity);
-//$pdf->addText(30,400,30,$dbDepartSt);
-//$pdf->addText(30,400,30,$dbDepartTime);
-//$pdf->addText(30,400,30,$dbArrivalCity);
-//$pdf->addText(30,400,30,$dbArrivalSt);
-//$pdf->addText(30,400,30,$dbflightDuration);
-
-//$pdf->ezstream();
-//while($data[]=mysqli_fetch_array($result)){}
-///in searchflights, take in users flight number, go through flights database and pull out the row, store each in session variables
-//$pdf->selectFont('./fonts/Helvetica');
-
-//$pdf->ezstream();
-//$pdf->addText(150,$y,10,"the quick brown fox <b>jumps</b>
-//<i>over</i> the lazy dog!",0, 'left',-10);
+//$flightNum="hello";
+$flightNum=$_SESSION['flight_num'];
+$departCity= $_SESSION['depart_city'];
+$departState=$_SESSION['depart_st'];
+$departTime=$_SESSION['depart_time'];
+$arrivalCity=$_SESSION['arrival_city'];
+$arrivalState=$_SESSION['arrival_st'];
+$flightDuration=$_SESSION['flight_duration'];
 
 
-//$pdf->selectFont('../src/fonts/Helvetica');
-//$pdf->ezText("Some annotations are only shown in Adobe Reader. Chrome Viewer for instance does not show the icons\n");
-//$pdf->ezTable($data);
-//$pdf->addText(30,400,30,"hello");
-//else
-//{
-//$result=mysqli_query($dbCon,'SELECT * FROM flights');
-// WHERE flight_num=$flightNum
-//while($data[]=mysqli_fetch_array($result)){}
-//$r=$row['flight_num']
-//$pdf->selectFont('./fonts/Helvetica');
-//$pdf->addText(30,400,30,$r);
-//$pdf->stream();
-//}
-//$pdf->ezTable($data);
+$pdf->selectFont('../src/fonts/Helvetica');
+$pdf->ezText("Flight Number:");
+$pdf->ezText($flightNum);
 
-if (isset($d) && $d){
-  $pdfcode = $pdf->output(1);
-  $pdfcode = str_replace("\n","\n<br>",htmlspecialchars($pdfcode));
-  echo '<html><body>';
-  echo trim($pdfcode);
-  echo '</body></html>';
-} else {
-  $pdf->ezStream();
-}
+$pdf->ezText("Departure City:");
+$pdf->ezText($departCity);
 
-	
+$pdf->ezText("Departure State:");
+$pdf->ezText($departState);
+
+$pdf->ezText("Departure Time:");
+$pdf->ezText($departTime);
+
+$pdf->ezText("Arrival City:");
+$pdf->ezText($arrivalCity);
+
+$pdf->ezText("Arrival State:");
+$pdf->ezText($arrivalState);
+
+$pdf->ezText("Flight Duration:");
+$pdf->ezText($flight_duration);
+
+
 
 if (isset($_GET['d']) && $_GET['d']){
   $pdfcode = $pdf->ezOutput(1);
@@ -105,6 +69,4 @@ if (isset($_GET['d']) && $_GET['d']){
 } else {
   $pdf->ezStream(array('compress'=>0));
 }
-
-
 ?>
