23908a10ad71d95c7bdb6a37d32e6d35366d6b75
amazzola <mazzolaamy@cox.net>
2013-11-18 21:27:26 -0700

    updated searchFlight

--- php/searchFlights.php
+++ php/searchFlights.php
@@ -6,47 +6,7 @@ session_start();
 	require_once("menu.html");
 	
 	
-$flight=mysqli_query($dbCon, "SELECT * FROM flights");
-	echo 	$_SESSION['username'];
-       echo $_SESSION['id'];
-	echo "<table border='1'>
-	<tr>
-	
-	<th>Flight Number</th>
-	<th>Departure City</th>
-	<th>Departure State</th>
-	<th>Departure Airport</th>
-	<th>Departure Time</th>
-	<th>Departure Time Zone</th>
-	<th>Arrival City</th>
-	<th>Arrival State</th>
-	<th>Arrival Airport</th>
-	<th>Arrival Time</th>
-	<th>Arrival Time Zone</th>
-	<th>Flight Duration</th>
-	<th>Seats Available</th>
 
-	</tr>";
-
-while($row = mysqli_fetch_array($flight))
-  {
-  echo "<tr>";
-  echo "<td>" . $row['flight_num'] . "</td>";
-  echo "<td>" . $row['depart_city'] . "</td>";
-  echo "<td>" . $row['depart_st'] . "</td>";
-  echo "<td>" . $row['depart_airport'] . "</td>";
-  echo "<td>" . $row['depart_time'] . "</td>";
-  echo "<td>" . $row['depart_tzone'] . "</td>";
-  echo "<td>" . $row['arrival_city'] . "</td>";
-  echo "<td>" . $row['arrival_st'] . "</td>";
-  echo "<td>" . $row['arrival_airport'] . "</td>";
-  echo "<td>" . $row['arrival_time'] . "</td>";
-  echo "<td>" . $row['arrival_tzone'] . "</td>";
-  echo "<td>" . $row['flight_duration'] . "</td>";
-   echo "<td>" . $row['flight_seats_available'] . "</td>";
-  echo "</tr>";
-  }
-echo "</table>";
 
 if(isset($_POST['flightNum'])) {
 	include_once("dbconnect.php");
