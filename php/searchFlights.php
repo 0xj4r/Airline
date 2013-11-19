30ca04f0d5473db0f76af2d28e40d19627f42cc0
amazzola <mazzolaamy@cox.net>
2013-11-19 00:27:36 -0700

    added pdf viewer

--- php/searchFlights.php
+++ php/searchFlights.php
@@ -19,6 +19,24 @@ if(isset($_POST['flightNum'])) {
 	
 	$userid=$_SESSION['id'];
 	
+	
+	$mysql = "SELECT depart_city, depart_st, depart_time, arrival_city, arrival_st, flight_duration FROM flights WHERE flight_num = '$flightNum' LIMIT 1";
+	$result = mysqli_query($dbCon, $mysql);
+	$row = mysqli_fetch_row($result);
+	$dbDepartCity = $row[0];
+	$dbDepartSt = $row[1];
+	$dbDepartTime = $row[2];
+    $dbArrivalCity = $row[3]; 
+	$dbArrivalSt= $row[4]; 
+	$dbFlightDuration=$row[5];
+	
+	$_SESSION['depart_city']=$dbDepartCity;
+	$_SESSION['depart_st']=$dbDepartSt;
+	$_SESSION['depart_time']=$dbDepartTime;
+	$_SESSION['arrival_city']=$dbArrivalCity;
+	$_SESSION['arrival_st']=$dbArrivalSt;
+	$_SESSION['flight_duration']=$dbFlightDuration;
+	
 	//set flight_booked in members database to flight number booked
 	mysqli_query($dbCon,"UPDATE members SET flight_booked='$flightNum' WHERE user_id='$userid'")
 	or die(mysql_error());
